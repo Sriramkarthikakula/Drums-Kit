e5e2b17e4d589cb5ffc9b1e4ce054256199ee7c3
Sriramkarthikakula <92425111+Sriramkarthikakula@users.noreply.github.com>
2022-10-27 21:12:03 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,40 @@
+for(i=0;i<document.querySelectorAll(".drum").length;i++)
+{
+    document.querySelectorAll(".drum")[i].addEventListener("click",clicked);
+}
+
+function clicked()
+{
+    var band = this.innerHTML;
+    switch(band)
+    {
+        case "w":
+            var audio = new Audio("tom-1.mp3");
+            audio.play();
+            break;
+        case "a":
+            var audio = new Audio("tom-2.mp3");
+            audio.play();
+            break;
+        case "s":
+            var audio = new Audio("tom-3.mp3");
+            audio.play();
+            break;
+        case "d":
+            var audio = new Audio("tom-4.mp3");
+            audio.play();
+            break;
+        case "j":
+            var audio = new Audio("snare.mp3");
+            audio.play();
+            break;
+        case "k":
+            var audio = new Audio("kick-bass.mp3");
+            audio.play();
+            break;
+        case "l":
+            var audio = new Audio("crash.mp3");
+            audio.play();
+            break;
+    }
+}
