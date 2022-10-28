35d0af673c4557e12d4e343f5a517d30b7b20ab9
Sriramkarthikakula <92425111+Sriramkarthikakula@users.noreply.github.com>
2022-10-28 20:49:37 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,35 +6,46 @@ for(i=0;i<document.querySelectorAll(".drum").length;i++)
 function clicked()
 {
     var band = this.innerHTML;
-    switch(band)
-    {
-        case "w":
-            var audio = new Audio("tom-1.mp3");
-            audio.play();
-            break;
-        case "a":
-            var audio = new Audio("tom-2.mp3");
-            audio.play();
-            break;
-        case "s":
-            var audio = new Audio("tom-3.mp3");
-            audio.play();
-            break;
-        case "d":
-            var audio = new Audio("tom-4.mp3");
-            audio.play();
-            break;
-        case "j":
-            var audio = new Audio("snare.mp3");
-            audio.play();
-            break;
-        case "k":
-            var audio = new Audio("kick-bass.mp3");
-            audio.play();
-            break;
-        case "l":
-            var audio = new Audio("crash.mp3");
-            audio.play();
-            break;
-    }
+    sound(band); 
+}
+
+document.addEventListener("keydown",pressed);
+
+function pressed(event){
+    sound(event.key);
+}
+
+function sound(keys){
+{
+    switch(keys)
+        {
+            case "w":
+                var audio = new Audio("tom-1.mp3");
+                audio.play();
+                break;
+            case "a":
+                var audio = new Audio("tom-2.mp3");
+                audio.play();
+                break;
+            case "s":
+                var audio = new Audio("tom-3.mp3");
+                audio.play();
+                break;
+            case "d":
+                var audio = new Audio("tom-4.mp3");
+                audio.play();
+                break;
+            case "j":
+                var audio = new Audio("snare.mp3");
+                audio.play();
+                break;
+            case "k":
+                var audio = new Audio("kick-bass.mp3");
+                audio.play();
+                break;
+            case "l":
+                var audio = new Audio("crash.mp3");
+                audio.play();
+                break;
+        }
 }
